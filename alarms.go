4eaa0ad5fd356212933a12f47b4541532adc9174
Fabio Berger <fabioberger1991@gmail.com>
2015-02-06 18:51:43 -0500

    Added documentation to Alarms

--- alarms.go
+++ alarms.go
@@ -10,23 +10,30 @@ type Alarms struct {
 * Methods:
  */
 
-// You must use time.Now().UnixNano() for "when" timestamp in alarmInfo
+// Create creates an alarm. Near the time(s) specified by alarmInfo, the onAlarm event is fired.
+// If there is another alarm with the same name (or no name if none is specified), it will be
+// cancelled and replaced by this alarm.
+// You must use time.Now().UnixNano() for "when" timestamp in alarmInfo for this to work.
 func (a *Alarms) Create(name string, alarmInfo interface{}) {
 	a.o.Call("create", name, alarmInfo)
 }
 
+// Get retrieves details about the specified alarm.
 func (a *Alarms) Get(name string, callback func(alarm map[string]interface{})) {
 	a.o.Call("get", name, callback)
 }
 
+// GetAll gets an array of all the alarms.
 func (a *Alarms) GetAll(callback func(alarms []map[string]interface{})) {
 	a.o.Call("getAll", callback)
 }
 
+// Clear clears the alarm with the given name.
 func (a *Alarms) Clear(name string, callback func(wasCleared bool)) {
 	a.o.Call("clear", name, callback)
 }
 
+// ClearAll clears all alarms.
 func (a *Alarms) ClearAll(callback func(wasCleared bool)) {
 	a.o.Call("clearAll", callback)
 }
@@ -34,7 +41,7 @@ func (a *Alarms) ClearAll(callback func(wasCleared bool)) {
 /*
 * Events
  */
-
+// OnAlarm is fired when an alarm has elapsed. Useful for event pages.
 func (a *Alarms) OnAlarm(callback func(map[string]interface{})) {
 	a.o.Get("onAlarm").Call("addListener", callback)
 }
