ce4450d2df5c43c7c821ea332265c98c46ba0601
Fabio Berger <fabioberger1991@gmail.com>
2015-02-07 14:09:15 -0500

    Added documentation to desktopCapture and declarativeContent

--- declarative-content.go
+++ declarative-content.go
@@ -15,14 +15,17 @@ type OnPageChanged struct {
 * Events
  */
 
+// AddRules takes an array of rule instances as its first parameter and a callback function that is called on completion
 func (e *OnPageChanged) AddRules(rules []map[string]interface{}, callback func(details map[string]interface{})) {
 	e.o.Call("addRules", rules, callback)
 }
 
+// RemoveRules accepts an optional array of rule identifiers as its first parameter and a callback function as its second parameter.
 func (e *OnPageChanged) RemoveRules(ruleIdentifiers []string, callback func()) {
 	e.o.Call("removeRules", ruleIdentifiers, callback)
 }
 
+// GetRules accepts an optional array of rule identifiers with the same semantics as removeRules and a callback function.
 func (e *OnPageChanged) GetRules(ruleIdentifiers []string, callback func(details map[string]interface{})) {
 	e.o.Call("getRules", ruleIdentifiers, callback)
 }
--- desktop-capture.go
+++ desktop-capture.go
@@ -10,10 +10,12 @@ type DesktopCapture struct {
 * Methods:
  */
 
+// ChooseDesktopMedia shows desktop media picker UI with the specified set of sources.
 func (d *DesktopCapture) ChooseDesktopMedia(sources []map[string]interface{}, targetTab map[string]interface{}, callback func(streamId string)) int {
 	return d.o.Call("chooseDesktopMedia", sources, targetTab, callback).Int()
 }
 
+// CancelChooseDesktopMedia hides desktop media picker dialog shown by chooseDesktopMedia().
 func (d *DesktopCapture) CancelChooseDesktopMedia(desktopMediaRequestId int) {
 	d.o.Call("cancelChooseDesktopMedia", desktopMediaRequestId)
 }
