4e5b0fa4d373c3444bbadbf61f9d9611c6aa7884
Fabio Berger <fabioberger1991@gmail.com>
2015-02-08 17:00:00 -0500

    Completed extensions API

--- extension.go
+++ extension.go
@@ -10,14 +10,36 @@ type Extension struct {
 * Methods:
  */
 
-func (e *Extension) SendRequest(extensionId string, request interface{}, responseCallback func(response interface{})) {
-	e.o.Call("sendRequest", extensionId, request, responseCallback)
-}
-
+// GetURL converts a relative path within an extension install directory to a fully-qualified URL.
 func (e *Extension) GetURL(path string) {
 	e.o.Call("getURL", path)
 }
 
+// GetViews returns an array of the JavaScript 'window' objects for each of the pages running inside the current extension.
 func (e *Extension) GetViews(fetchProperties map[string]interface{}) interface{} {
 	return e.o.Call("getViews", fetchProperties).Interface()
+}
+
+// GetBackgroundPage returns the JavaScript 'window' object for the background page running inside
+// the current extension. Returns null if the extension has no background page.
+func (e *Extension) GetBackgroundPage() interface{} {
+	return e.o.Call("getBackgroundPage").Interface()
+}
+
+// IsAllowedIncognitoAccess retrieves the state of the extension's access to Incognito-mode
+// (as determined by the user-controlled 'Allowed in Incognito' checkbox.
+func (e *Extension) IsAllowedIncognitoAccess(callback func(isAllowedAccess bool)) {
+	e.o.Call("isAllowedIncognitoAccess", callback)
+}
+
+// IsAllowedFileSchemeAccess retrieves the state of the extension's access to the 'file://'
+// scheme (as determined by the user-controlled 'Allow access to File URLs' checkbox.
+func (e *Extension) IsAllowedFileSchemeAccess(callback func(isAllowedAccess bool)) {
+	e.o.Call("isAllowedFileSchemeAccess", callback)
+}
+
+// SetUpdateUrlData sets the value of the ap CGI parameter used in the extension's update URL.
+// This value is ignored for extensions that are hosted in the Chrome Extension Gallery.
+func (e *Extension) SetUpdateUrlData(data string) {
+	e.o.Call("setUpdateUrlData", data)
 }
