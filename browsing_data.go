1fe1198b074ec0a31ba4cb7387ccb4fa2069fb06
Fabio Berger <fabioberger1991@gmail.com>
2015-02-09 09:58:55 -0500

    Added types to browsingData

--- browsing_data.go
+++ browsing_data.go
@@ -7,6 +7,18 @@ type BrowsingData struct {
 }
 
 /*
+* Types
+ */
+
+type RemovalOptions struct {
+	js.Object
+	Since       float64         `js:"since,omitempty"`
+	OriginTypes map[string]bool `js:"originTypes,omitempty"`
+}
+
+type DataTypeSet map[string]bool
+
+/*
 * Methods:
  */
 
@@ -18,66 +30,66 @@ func (b *BrowsingData) Settings(result map[string]interface{}) {
 }
 
 // Remove clears various types of browsing data stored in a user's profile.
-func (b *BrowsingData) Remove(options map[string]interface{}, dataToRemove map[string]interface{}, callback func()) {
+func (b *BrowsingData) Remove(options RemovalOptions, dataToRemove DataTypeSet, callback func()) {
 	b.o.Call("remove", options, dataToRemove, callback)
 }
 
 // RemoveAppCache clears websites' appcache data.
-func (b *BrowsingData) RemoveAppCache(options map[string]interface{}, callback func()) {
+func (b *BrowsingData) RemoveAppCache(options RemovalOptions, callback func()) {
 	b.o.Call("removeAppCache", options, callback)
 }
 
 // RemoveCache clears the browser's cache.
-func (b *BrowsingData) RemoveCache(options map[string]interface{}, callback func()) {
+func (b *BrowsingData) RemoveCache(options RemovalOptions, callback func()) {
 	b.o.Call("removeCache", options, callback)
 }
 
 // RemoveCookies clears the browser's cookies and server-bound certificates modified within a particular timeframe.
-func (b *BrowsingData) RemoveCookies(options map[string]interface{}, callback func()) {
+func (b *BrowsingData) RemoveCookies(options RemovalOptions, callback func()) {
 	b.o.Call("removeCookies", options, callback)
 }
 
 // RemoveDownloads clears the browser's list of downloaded files (not the downloaded files themselves).
-func (b *BrowsingData) RemoveDownloads(options map[string]interface{}, callback func()) {
+func (b *BrowsingData) RemoveDownloads(options RemovalOptions, callback func()) {
 	b.o.Call("removeDownloads", options, callback)
 }
 
 // RemoveFileSystems clears websites' file system data.
-func (b *BrowsingData) RemoveFileSystems(options map[string]interface{}, callback func()) {
+func (b *BrowsingData) RemoveFileSystems(options RemovalOptions, callback func()) {
 	b.o.Call("removeFileSystems", options, callback)
 }
 
 // RemoveFormData clears the browser's stored form data (autofill).
-func (b *BrowsingData) RemoveFormData(options map[string]interface{}, callback func()) {
+func (b *BrowsingData) RemoveFormData(options RemovalOptions, callback func()) {
 	b.o.Call("removeFormData", options, callback)
 }
 
 // RemoveHistory clears the browser's history.
-func (b *BrowsingData) RemoveHistory(options map[string]interface{}, callback func()) {
+func (b *BrowsingData) RemoveHistory(options RemovalOptions, callback func()) {
 	b.o.Call("removeHistory", options, callback)
 }
 
 // RemoveIndexedDB clears websites' IndexedDB data.
-func (b *BrowsingData) RemoveIndexedDB(options map[string]interface{}, callback func()) {
+func (b *BrowsingData) RemoveIndexedDB(options RemovalOptions, callback func()) {
 	b.o.Call("removeIndexedDB", options, callback)
 }
 
 // RemoveLocalStorage clears websites' local storage data.
-func (b *BrowsingData) RemoveLocalStorage(options map[string]interface{}, callback func()) {
+func (b *BrowsingData) RemoveLocalStorage(options RemovalOptions, callback func()) {
 	b.o.Call("removeLocalStorage", options, callback)
 }
 
 // RemovePluginData clears plugins' data.
-func (b *BrowsingData) RemovePluginData(options map[string]interface{}, callback func()) {
+func (b *BrowsingData) RemovePluginData(options RemovalOptions, callback func()) {
 	b.o.Call("removePluginData", options, callback)
 }
 
 // RemovePasswords clears the browser's stored passwords.
-func (b *BrowsingData) RemovePasswords(options map[string]interface{}, callback func()) {
+func (b *BrowsingData) RemovePasswords(options RemovalOptions, callback func()) {
 	b.o.Call("removePasswords", options, callback)
 }
 
 // RemoveWebSQL clears websites' WebSQL data.
-func (b *BrowsingData) RemoveWebSQL(options map[string]interface{}, callback func()) {
+func (b *BrowsingData) RemoveWebSQL(options RemovalOptions, callback func()) {
 	b.o.Call("removeWebSQL", options, callback)
 }
